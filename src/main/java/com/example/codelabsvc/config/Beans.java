4337a1c131bc2be54749c88e6fd4b96565ecfe90
hoangkm13 <hoang.workingemail@gmail.com>
2023-05-22 17:45:19 +0700

    add open api

--- src/main/java/com/example/codelabsvc/config/Beans.java
+++ src/main/java/com/example/codelabsvc/config/Beans.java
@@ -1,10 +1,17 @@
 package com.example.codelabsvc.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
 
 @Configuration
 public class Beans {
@@ -18,14 +25,14 @@ public class Beans {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .components(new Components().addSecuritySchemes("bearer-jwt",
-//                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-//                                .in(SecurityScheme.In.HEADER).name("Authorization")))
-//                .info(new Info().title("App API").version("snapshot"))
-//                .addSecurityItem(
-//                        new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")));
-//    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearer-jwt",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER).name("Authorization")))
+                .info(new Info().title("App API").version("snapshot"))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")));
+    }
 }
