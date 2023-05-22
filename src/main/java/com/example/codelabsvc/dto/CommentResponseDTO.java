3efc586e19c300e944e94777d1a772e5c9998cce
thanhlam0822 <benzamano123@gmail.com>
2023-05-22 21:47:39 +0700

    refactor code and remove unneed entity and service

---
+++ src/main/java/com/example/codelabsvc/dto/CommentResponseDTO.java
@@ -0,0 +1,22 @@
+package com.example.codelabsvc.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CommentResponseDTO {
+    private String id;
+    private String username;
+    private String createdAt;
+    private String text;
+    private String code;
+
+    public CommentResponseDTO(String id, String username, String createdAt, String text, String code) {
+        this.id = id;
+        this.username = username;
+        this.createdAt = createdAt;
+        this.text = text;
+        this.code = code;
+    }
+}
