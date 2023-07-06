d4563ce18aec499eab025ca571fb56357108542b
VietTruong-50 <truongquocviet2001@gmail.com>
2023-07-06 08:59:06 +0700

    refactor

---
+++ src/main/java/com/example/codelabsvc/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.example.codelabsvc.service;
+
+import com.example.codelabsvc.controller.response.comment.SaveNotificationRequestDTO;
+import com.example.codelabsvc.entity.Notification;
+import com.example.codelabsvc.exception.CustomException;
+import org.springframework.data.domain.Page;
+
+public interface NotificationService {
+    Notification saveNotification(SaveNotificationRequestDTO dto) throws CustomException;
+    Notification updateNotification(String notificationId);
+    Page<Notification>  getAllNotification(String userId,int page,int size);
+}
