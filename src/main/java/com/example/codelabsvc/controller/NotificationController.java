24bbfac086093ed8bb47c2949bd550b1e47499e0
VietTruong-50 <truongquocviet2001@gmail.com>
2023-07-11 22:47:05 +0700

    refactor notification

--- src/main/java/com/example/codelabsvc/controller/NotificationController.java
+++ src/main/java/com/example/codelabsvc/controller/NotificationController.java
@@ -1,12 +1,15 @@
 package com.example.codelabsvc.controller;
 
 import com.example.codelabsvc.controller.response.comment.SaveNotificationRequestDTO;
+import com.example.codelabsvc.controller.response.notification.NotificationResponse;
 import com.example.codelabsvc.entity.Notification;
 import com.example.codelabsvc.exception.CustomException;
 import com.example.codelabsvc.model.ApiResponse;
 import com.example.codelabsvc.service.NotificationService;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/notification")
@@ -23,16 +26,15 @@ public class NotificationController {
         return ApiResponse.successWithResult(notificationService.saveNotification(dto));
     }
 
-    @PostMapping("{notificationId}")
+    @PutMapping("{notificationId}")
     public ApiResponse<Notification>  updateNotification(@PathVariable String notificationId) {
         return ApiResponse.successWithResult(notificationService.updateNotification(notificationId));
     }
 
-    @GetMapping("{userId}")
-    public ApiResponse<Page<Notification>> getAllNotification(@PathVariable String userId,
-                                                         @RequestParam(required = false, defaultValue = "1") int page,
-                                                         @RequestParam(required = false, defaultValue = "5") int size) {
-        return ApiResponse.successWithResult(notificationService.getAllNotification(userId, page, size));
+    @GetMapping("")
+    public ApiResponse<Page<NotificationResponse>> getAllNotification(@RequestParam(required = false, defaultValue = "1") int page,
+                                                                      @RequestParam(required = false, defaultValue = "5") int size) {
+        return ApiResponse.successWithResult(notificationService.getAllNotification(page, size));
     }
 
     @DeleteMapping("{notificationId}")
